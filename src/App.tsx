67342582a760da19f279ce6a0c02e83f437977a2
Sardius <0xsardius@gmail.com>
2024-10-31 07:33:00 -0400

    complete fetch example

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const BASE_URL = "https://jsonplaceholder.typicode.com";
 
@@ -10,30 +10,56 @@ type Post = {
 
 function App() {
   const [posts, setPosts] = useState<Post[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [page, setPage] = useState(0);
+
+  const abortControllerRef = useRef<AbortController | null>(null);
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const response = await fetch(`${BASE_URL}/posts`);
-      const posts = (await response.json()) as Post[];
-      setPosts(posts);
+      // Abort request if it exists
+      abortControllerRef.current?.abort();
+      abortControllerRef.current = new AbortController();
+
+      setIsLoading(true);
+
+      try {
+        const response = await fetch(`${BASE_URL}/posts?page=${page}`, {
+          signal: abortControllerRef.current?.signal,
+        });
+        const posts = (await response.json()) as Post[];
+        setPosts(posts);
+      } catch (error: any) {
+        if (error.name === "AbortError") {
+          console.log("Request aborted");
+          return;
+        }
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
     };
     fetchPosts();
-  }, []);
+  }, [posts]);
 
   return (
     <>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        {posts.map((post) => (
-          <div
-            key={post.id}
-            className="bg-white shadow-lg rounded-lg p-6 mb-6 hover:shadow-xl transition duration-300"
-          >
-            <h2 className="text-2xl font-bold text-gray-800 mb-4">
-              {post.title}
-            </h2>
-            <p className="text-gray-600 leading-relaxed">{post.body}</p>
-          </div>
-        ))}
+        <button onClick={() => setPage(page + 1)}>Increase Page {page}</button>
+        {isLoading && <div>Loading...</div>}
+        {!isLoading &&
+          posts.map((post) => (
+            <div
+              key={post.id}
+              className="bg-white shadow-lg rounded-lg p-6 mb-6 hover:shadow-xl transition duration-300"
+            >
+              <h2 className="text-2xl font-bold text-gray-800 mb-4">
+                {post.title}
+              </h2>
+              <p className="text-gray-600 leading-relaxed">{post.body}</p>
+            </div>
+          ))}
       </div>
     </>
   );
